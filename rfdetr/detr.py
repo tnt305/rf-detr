b4bb1ea840acdfa0be64527f7ab9439b7fc3b063
Thien Dang <110881337+tnt305@users.noreply.github.com>
2025-11-07 19:42:58 +0700

    Refactor predictions handling in inference model

--- rfdetr/detr.py
+++ rfdetr/detr.py
@@ -316,12 +316,17 @@ class RFDETR:
             else:
                 predictions = self.model.model(batch_tensor)
             if isinstance(predictions, tuple):
-                predictions = {
-                    "pred_logits": predictions[1],
-                    "pred_boxes": predictions[0],
-                }
                 if len(predictions) == 3:
-                    predictions["pred_masks"] = predictions[2]
+                        predictions = {
+                        "pred_boxes": predictions[0],
+                        "pred_logits": predictions[1],
+                        "pred_masks": predictions[2],
+                    }
+                else:
+                    predictions = {
+                        "pred_boxes": predictions[0],
+                        "pred_logits": predictions[1],
+                    }
             target_sizes = torch.tensor(orig_sizes, device=self.model.device)
             results = self.model.postprocess(predictions, target_sizes=target_sizes)
 
